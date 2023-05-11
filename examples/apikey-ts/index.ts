f820b333fa7617d542ec9b45760e58c7805731b6
Piers Karsenbarg <piers@pulumi.com>
2023-05-11 14:23:44 +0100

    Fixing nodejs import statement

--- examples/apikey-ts/index.ts
+++ examples/apikey-ts/index.ts
@@ -1,4 +1,4 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as ngrok from "@pierskarsenbarg/pulumi-ngrok";
+import * as ngrok from "@pierskarsenbarg/ngrok";
 
 const apikey = new ngrok.ApiKey("apikey");
